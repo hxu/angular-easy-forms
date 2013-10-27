0c96fcfce747fbd4e439d29c9d3339e34da712d4
Han Xu <hgcrpd@gmail.com>
2013-10-27 00:16:36 -0700

    ...

--- test/spec/easyForm.js
+++ test/spec/easyForm.js
@@ -1,8 +1,6 @@
 'use strict';
 
 describe('easyForm service', function() {
-  var easyForm, $compile, $rootScope;
-
 
   describe('config defaults', function() {
     beforeEach(module('easyForms', function(easyFormProvider) {
@@ -15,7 +13,6 @@ describe('easyForm service', function() {
       scope.$initialize('foo', {});
       expect(scope.efConfig.successMessage).toEqual('foo!');
     }));
-
   });
 
   it('should extend a scope with form handling functions', function () {
