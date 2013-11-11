4d268c758dce940690486949a231d2febf728000
Han Xu <hgcrpd@gmail.com>
2013-11-11 15:21:26 -0800

    update

--- angular-easy-forms.js
+++ angular-easy-forms.js
@@ -162,8 +162,10 @@ angular.module('easyForms').
   directive('efMessages', function() {
     return {
       replace: true,
-      template: '<div class="alert alert-{{ m.class }}" ng-show="hasMessages()">' +
-        '<div ng-repeat="m in messages">{{ m.text }}</div>' +
+      template: '<div ng-show="hasMessages()">' +
+        '<div ng-repeat="m in messages" class="alert alert-{{ m.class }}">' +
+        '{{ m.text }}' +
+        '</div>' +
         '</div>'
     };
   });
