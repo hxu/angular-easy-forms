379ed7ca733883147314f1b8f052d3cae4ece80d
Han Xu <hgcrpd@gmail.com>
2013-12-05 00:11:50 -0800

    Make note of timing bug

--- src/directives/efForm.js
+++ src/directives/efForm.js
@@ -23,6 +23,10 @@ angular.module('easyForms').
         // Extend and initialize the scope
         easyForm.extendScope(scope);
         scope.$initialize(scope.efResource, attrs);
+        // This watch actually NEEDS to be here.  There is an issue with timing where some of the components on the scope
+        // Are not ready yet if you just $initialize.  You need to $initialize once, so that the messages and model objects
+        // Exist on the scope (otherwise you will get some harmless errors), then you need to initialize again when the scope is
+        // ready.
         scope.$watch('efResource', function(newVal) {
           scope.$initialize(newVal, attrs);
         });
