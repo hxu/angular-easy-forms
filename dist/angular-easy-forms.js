adb2b10e8c4bdb893993326e8cfae127039135b1
Han Xu <hgcrpd@gmail.com>
2013-12-07 03:47:43 -0800

    build [ci skip]

--- dist/angular-easy-forms.js
+++ dist/angular-easy-forms.js
@@ -1,6 +1,6 @@
 /**
  * AngularEasyForms - Basic CRUD operation handling in a box
- * @version v0.1.0 - 2013-11-13
+ * @version v0.1.0 - 2013-12-07
  * @link http://github.com/hxu/angular-easy-forms
  * @author Han Xu
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -149,6 +149,10 @@ angular.module('easyForms').
         // Extend and initialize the scope
         easyForm.extendScope(scope);
         scope.$initialize(scope.efResource, attrs);
+        // This watch actually NEEDS to be here.  There is an issue with timing where some of the components on the scope
+        // Are not ready yet if you just $initialize.  You need to $initialize once, so that the messages and model objects
+        // Exist on the scope (otherwise you will get some harmless errors), then you need to initialize again when the scope is
+        // ready.
         scope.$watch('efResource', function(newVal) {
           scope.$initialize(newVal, attrs);
         });
